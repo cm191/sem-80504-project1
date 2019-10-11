ecc7d9395db67876171b2acd4190df8e857165b3
cm191 <cm191@students.uwf.edu>
2019-10-11 17:56:13 -0500

    modified to see if it works now

--- routes/version.js
+++ routes/version.js
@@ -3,6 +3,8 @@ let fs = require('fs');
 
 const router = Router();
 
-router.get('/', (req, res) => res.send('This is version 0 of the HotBurger service'));
+router.get('/', (req, res) => {
+    res.send('This is version 0 of the HotBurger service')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
-const version = require('./routes/version');
+const routes = require('./routes');
 
 server.get('/', (req, res) => res.send('hello'));
-server.use('/version', version);
+server.use('/', routes);
 
 server.listen(port);
