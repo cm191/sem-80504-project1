bb0e9e34f645eb18d286441974d5ee059202104d
cm191 <cm191@students.uwf.edu>
2019-10-11 18:15:59 -0500

    added logs route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const router = Router();
+
 const version = require('./version');
+const logs = require('./logs');
 
 router.get('/', (req, res) => {
 	res.send('hello\n');
 });
 
 router.use('/version', version);
+router.use('/logs', logs);
 
 module.exports = router;
